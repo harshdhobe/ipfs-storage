535e30fd991a855987fc8a8c74d2936bd2ba401c
Harsh Dhobe <harshdhobe123@gmail.com>
2024-12-13 21:00:08 +0530

    add changehandler function

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,13 @@ const App = () => {
   const [ipfsHash, setipfsHash] = useState("");
   const [storedHash, setstoredHash] = useState("");
 
-  const contractAddress = "0xd9145CCE52D386f254917e481eB44e9943F39138",
+  const changeHandler = (event) => {
+    setselectedFile(event.target.files[0]);
+  }
+  
+  
+
+  const contractAddress = "0xd9145CCE52D386f254917e481eB44e9943F39138";
   const contractAbi = [
     {
       "inputs": [
@@ -41,7 +47,16 @@ const App = () => {
   ]
 
   return (
-    
+    <div className='app-container'>
+      <div className='upload-section'>
+        <label className='label-name'>IPFS Storage</label>
+        <input type='file' className='file-input' onChange={changeHandler}></input>
+        <button className='submit-button' onClick={handleSubmission}>Submit</button>
+
+      </div>
+
+    </div>
+
   )
 }
 
