13b09172b5b07bf112465a32ed826b1a0fcf9330
Harsh Dhobe <harshdhobe123@gmail.com>
2024-12-13 20:08:23 +0530

    add contarct address and contract abi of deployed contract

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import { pinata } from '../config'
+import {ethers} from "ethers"
+
+import './App.css'
+
+const App = () => {
+
+  const contractAddress ="0xd9145CCE52D386f254917e481eB44e9943F39138",
+  const contractAbi = [
+    {
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "_ipfshash",
+          "type": "string"
+        }
+      ],
+      "name": "setIPFSHash",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "getIPFSHash",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    }
+  ]
+  
+  return (
+    
+  )
+}
+
+export default App
+
+
